ecdc520a9a64cc9d1e0dc4dc4455c57d4ced9211
Laura Porcelli <lapo1002@ek.stud.dk>
2026-09-25 10:55:47 +0200

    style

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const endpoint = `https://kea-alt-del.dk/t7/api/products?category=${cat}`;
 
 const liste = document.querySelector(".liste");
 
+const h2 = document.querySelector("h2");
+h2.textContent = cat;
+
 fetch(endpoint)
   .then((res) => res.json())
   .then(visData);
@@ -13,10 +16,10 @@ function visData(json) {
     liste.innerHTML += `<a href=produktdetaljer.html?id=${element.id}> <article class="card">
     <img src=https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp />
     
-            <h2>${element.productdisplayname}</h2>
-            <h3>${element.price}</h3>
-            <p>${element.id}</p>
-            <p>${element.productionyear}</p>
+            <h2>${element.brandname}</h2>
+            <h3>${element.productdisplayname}</h3>
+            <p>${element.price}</p>
+            <p>${element.subcategory}</p>
         </article></a>`;
   });
 }
